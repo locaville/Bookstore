e97be4323c5f2a8de9a512143beda1c2c84ed303
Valtteri Puhakka <valtteri.puhakka@myy.haaga-helia.fi>
2017-09-18 16:08:31 +0300

    lisätty RESTful service kaikkien kirjojen hakemiseksi ja yhden kirjan hakemiseksi id:n perusteella

--- src/main/java/fi/haagahelia/okt/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/haagahelia/okt/Bookstore/BookstoreApplication.java
@@ -22,6 +22,8 @@ public class BookstoreApplication {
 			@Override
 			public void run(String... arg0) throws Exception {
 				repo.save(new Book("1984", "George Orwell", 1949, "951-0-23801-5", 40.0));
+				repo.save(new Book("The Brothers Karamazov", "Fedor Dostojevski", 1880, "978-951-23-5055-1", 59.90));
+				repo.save(new Book("Slaughterhouse 5", "Kurt Vonnegut", 1969, "0-385-31208-3", 24.90));
 			}
 			
 		};
--- src/main/java/fi/haagahelia/okt/Bookstore/domain/BookController.java
+++ src/main/java/fi/haagahelia/okt/Bookstore/domain/BookController.java
@@ -1,15 +1,18 @@
 package fi.haagahelia.okt.Bookstore.domain;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import fi.haagahelia.okt.Bookstore.model.Book;
 
-@Controller
+@RestController
 
 public class BookController {
 
@@ -38,6 +41,16 @@ public class BookController {
 	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
 		repository.delete(bookId);
 		return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value="/booksjson")
+	public List<Book> allBooksJSON(){
+		return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value="/bookjson/{id}", method = RequestMethod.GET)
+	public Book oneBookJSON(@PathVariable("id") Long bookId) {
+		return repository.findOne(bookId);
 	}
 
 }
